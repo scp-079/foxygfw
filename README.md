c0ad6374ed9a81eff50f3dfa4b5b040d7acd57b3
Xiao <xiao@scp-079.org>
2020-03-26 15:40:43 +0000

    commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# FoxyGFW
+
+适用人群：Firefox 上的 FoxyProxy 插件用户。
+
+## 简介
+
+Firefox 上的插件 [FoxyProxy](https://addons.mozilla.org/firefox/addon/foxyproxy-standard/) 改版后，目前只允许导入 `json` 格式的规则文件，由此给使用 [GFWList](https://github.com/gfwlist/gfwlist) 进行自动代理带来了[不便](https://github.com/gfwlist/gfwlist/issues/1892)。
+
+此程序实现的功能很简单，用于从 GFWList 中生成一份 `json` 文件，将其导入到 FoxyProxy 后，可实现类似于 SwitchyOmega 插件的 Auto Switch 功能。
+
+注意：由于 FoxyProxy 的规则只能针对域名进行匹配，故 GFWList 中涉及网址具体路径的规则无法发挥其效果，不过好在这类规则数量较少，而本程序的策略是对涉及路径的规则直接采用其域名，将域名添加到自动代理的列表中。
+
+## 使用方法
+
+以 `Debian` 系统为例：
+
+```bash
+sudo apt update
+sudo apt install git python3-venv -y
+```
+
+在终端中执行：
+
+```bash
+git clone https://github.com/scp-079/foxygfw.git foxygfw
+cd foxygfw
+cp config.ini.example config.ini
+```
+
+随后根据需要编辑 `config.ini` 文件，可设置是否使用代理获取 GFWList 列表，以及指定使用的 GFWList 列表的网址。保存后，执行：
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt    # 或 pip install -U requests[socks]
+python main.py
+deactivate
+```
+
+如程序正常执行，将在 `foxygfw` 文件夹下生成一个 `output.json` 文件。
+
+打开 Firefox，在 FoxyProxy 中选择 `Options`：
+
+![](images/start.png)
+
+在设置页面中：
+
+1. 选择 `Use Enabled Proxies By Patterns and Order`
+2. 添加一个代理，此处命名为 `Auto`，作为你想要自动使用的那个代理
+3. 关闭其余代理的开关按钮
+4. 编辑 `Auto` 的 `Patterns`
+
+![](images/options.png)
+
+点击导入，选择刚刚通过程序生成的 `output.json` 文件，导入后记得点击保存。
+
+![](images/import.png)
+
+设置已结束，Firefox 到此即实现对需要代理的网站自动代理了。此设置不影响对其他代理的单独使用，你仍可点击扩展的托盘图标，选择仅使用某个代理，如果之后再需自动代理，则切换回 `Use Enabled Proxies By Patterns and Order` 即可。
+
+你可能会发现每次进行导入操作后，都会替换原有的规则。那如何添加自定义的代理规则呢？
+
+根据 FoxyProxy 的逻辑，代理的选择是自上而下的，如果 `Auto` 匹配不到，则它会尝试其下方的代理设置，因此，可在 `Auto` 下面的位置再添加一个 `Custom` 代理，在 `Custom` 中添加自定义的规则即可。 
+
+## 参考
+
+FoxyProxy Pattern Help（扩展自带帮助页面）
+
+[Syntax · gfwlist/gfwlist Wiki](https://github.com/gfwlist/gfwlist/wiki/Syntax)
+
+[gfwlist / apollyon](https://github.com/gfwlist/apollyon/blob/master/checkRules.py)
---
+++ main.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# FoxyGFW - Generate GFWList rules file for FoxyProxy extension
+# Copyright (C) 2019-2020 SCP-079 <https://scp-079.org>
+#
+# This file is part of FoxyGFW.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from base64 import b64decode
+from configparser import RawConfigParser
+from json import load, dump
+from re import sub
+from requests import get
+from typing import Union
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.WARNING,
+    filename="log",
+    filemode="w"
+)
+logger = logging.getLogger(__name__)
+
+# Read data from config.ini
+
+# [proxy]
+enabled: Union[bool, str] = ""
+hostname: str = ""
+port: str = ""
+
+# [custom]
+url: str = ""
+
+try:
+    config = RawConfigParser()
+    config.read("config.ini")
+
+    # [proxy]
+    enabled = config["proxy"].get("enabled", enabled)
+    enabled = eval(enabled)
+    hostname = config["proxy"].get("hostname", hostname)
+    port = config["proxy"].get("port", port)
+
+    # [custom]
+    url = config["custom"].get("url", url)
+except Exception as e:
+    logger.warning(f"Read data from config.ini error: {e}", exc_info=True)
+
+# Check
+if (enabled not in {False, True}
+        or hostname == ""
+        or port == ""
+        or url == ""):
+    logger.critical("No proper settings")
+    raise SystemExit("No proper settings")
+
+# Set proxy
+if enabled:
+    proxies = {
+        "http": f"socks5://{hostname}:{port}",
+        "https": f"socks5://{hostname}:{port}"
+    }
+else:
+    proxies = None
+
+# Read base.json
+
+with open("base.json") as f:
+    base = load(f)
+
+base["whitePatterns"] = []
+
+
+def get_dict(rule: str) -> (int, dict):
+    # Get pattern dict
+    try:
+        protocol = 1
+        white = 1
+
+        # White rule
+        if rule.startswith("@@"):
+            rule = rule[2:]
+        else:
+            white = 0
+
+        # Get protocol
+        if rule.startswith("|http://"):
+            protocol = 2
+            rule = sub("^\\|http://", "|", rule)
+        elif rule.startswith("|https://"):
+            protocol = 4
+            rule = sub("^\\|https://", "||", rule)
+
+        # Get match type
+        if rule.startswith("||"):
+            rule = f"*.{rule[2:]}"
+        elif rule.startswith("|"):
+            rule = rule[1:]
+
+        # Format the rule
+        rule = rule.split("/")[0]
+
+        if "." not in rule:
+            rule = f"*{rule}*"
+
+        the_dict = {
+            "title": rule,
+            "pattern": rule,
+            "type": 1,
+            "protocols": protocol,
+            "active": True
+        }
+
+        return white, the_dict
+    except Exception as ee:
+        logger.warning(f"Get dict error: {ee}", exc_info=True)
+
+    return 0, {}
+
+
+def main():
+    try:
+        result = get(url, proxies=proxies)
+
+        if not result or not result.content:
+            return True
+
+        text = b64decode(result.content)
+        text = text.decode("utf-8")
+
+        if not text:
+            return True
+
+        rules = list(filter(None, text.split("\n")))
+
+        for rule in rules:
+            if rule.startswith("[AutoProxy"):
+                continue
+
+            if rule.startswith("!"):
+                continue
+
+            if rule.startswith("/") and rule.endswith("/"):
+                continue
+
+            white, the_dict = get_dict(rule)
+
+            if not the_dict:
+                continue
+
+            if white:
+                base["blackPatterns"].append(the_dict)
+            else:
+                base["whitePatterns"].append(the_dict)
+
+        with open("output.json", "w") as ff:
+            dump(base, ff, indent=4)
+    except Exception as ee:
+        logger.warning(f"Error: {ee}", exc_info=True)
+
+
+if __name__ == "__main__":
+    main()
